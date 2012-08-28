38bc919e7ea5fa3db6a8e047b325a850281fd1d4
Karell Ste-Marie <karell.stemarie@gmail.com>
2012-08-28 15:59:49 -0400

    Initial work done - now just need to test it

---
+++ log4net.aws/SimpleDBAppender.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using log4net.Appender;
+using log4net.Core;
+
+namespace log4net.aws
+{
+    public class SimpleDBAppender : BufferingAppenderSkeleton
+    {
+        /// <summary>
+        /// Sends the events.
+        /// </summary>
+        /// <param name="events">The events that need to be send.</param>
+        /// <remarks>
+        /// <para>
+        /// The subclass must override this method to process the buffered events.
+        /// </para>
+        /// </remarks>
+        protected override void SendBuffer(LoggingEvent[] events)
+        {
+            var client = new AmazonSimpleDBClient();
+            var request = new PutAttributesRequest();
+            foreach (LoggingEvent loggingEvent in events)
+            {
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Name = "UserName",
+                            Replace = true,
+                            Value = loggingEvent.UserName
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.TimeStamp.ToString(CultureInfo.InvariantCulture),
+                            Name = "TimeStamp",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.ThreadName,
+                            Name = "ThreadName",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.RenderedMessage,
+                            Name = "Message",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.LoggerName,
+                            Name = "LoggerName",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.Level.ToString(),
+                            Name = "Level",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.Identity,
+                            Name = "Identity",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = loggingEvent.Domain,
+                            Name = "Domain",
+                            Replace = true
+                        });
+                request.Attribute.Add(
+                    new ReplaceableAttribute
+                        {
+                            Value = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture),
+                            Name = "CreatedOn",
+                            Replace = true
+                        });
+                request.DomainName = "karelllogging";
+            }
+            PutAttributesResponse response = client.PutAttributes(request);
+        }
+    }
+}
